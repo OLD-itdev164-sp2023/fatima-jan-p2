6233584491cf4b3f97bf0b7a819a98a4e23258d4
janf319 <janf@gmatc.matc.edu>
2023-03-23 08:45:26 -0500

    gatsby-node defer removed

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -5,32 +5,31 @@ exports.createPages = async ({ graphql, actions }) => {
 
   return new Promise((resolve, reject) => {
     graphql(`
-        {
-          allContentfulBlogPost {
-            edges {
-              node {
-                id
-                slug
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
       }
-    }
-  }
-}
-`).then(result => {
-  if (result.errors) {
-    reject(result.errors)
-  }
-  result.data.allContentfulBlogPost.edges.forEach((edge) => {
-  console.log('slug value:', edge.node.slug);
-  createPage({
-    path: edge.node.slug,
-    component: require.resolve("./src/templates/blog-post.js"),
-    context: { 
-      slug: edge.node.slug
-    },
-    defer: true,
-  })
-})
-resolve()
-  })
+    `).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+      result.data.allContentfulBlogPost.edges.forEach((edge) => {
+        console.log('slug value:', edge.node.slug);
+        createPage({
+          path: edge.node.slug,
+          component: require.resolve("./src/templates/blog-post.js"),
+          context: { 
+            slug: edge.node.slug
+          }
+        })
+      })
+      resolve()
+    })
   })
 }
